0f352cd1c457001b0726f866115fa022e90eb579
SamSSY <sam7822567@gmail.com>
2015-12-10 07:57:42 +0800

    1210/0757

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -2,4 +2,7 @@ class CatalogController < ApplicationController
   def purchase
   	@id = params[:id]
   end
+  def sell
+  	@id = params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'products/:id/sell' => 'catalog#sell', as: :sell
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :products
+  resources :tests
 
   # Example resource route with options:
   #   resources :products do
